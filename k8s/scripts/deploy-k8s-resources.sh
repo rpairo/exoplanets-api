d62a5d7901f24fb7fc12b806539559364fcc4263
Raul Pera Pairo <raul.perapairo@gendigital.com>
2025-01-01 19:30:07 +0100

    New docker image name

--- k8s/scripts/deploy-k8s-resources.sh
+++ k8s/scripts/deploy-k8s-resources.sh
@@ -12,24 +12,27 @@ if [[ "$current_context" == "docker-desktop" ]]; then
     echo "AWS CLI not found. Creating secrets manually..."
     ./k8s/scripts/create-k8s-secrets-manual.sh
   fi
-
 else
   echo "Not running on Docker Desktop. Skipping secrets creation."
+  echo "I haven't been able to test in real cluster environment, I haven't been able to test properly the aws secrets integration."
+  echo "Since I haven't been able to test it, we will set up the credentials manually in this ocassion."
+  echo "AWS CLI not found. Creating secrets manually..."
+  ./k8s/scripts/create-k8s-secrets-manual.sh
 fi
 
-echo "Deploying Kubernetes resources..."
-
+echo "Cleaning Kubernetes resources..."
 kubectl delete deployment exoplanets-api-deployment
 kubectl delete service exoplanets-api-service
 
+echo "Deploying Kubernetes resources..."
 kubectl apply -f k8s/base/exoplanets-api-deployment.yaml
-kubectl apply -f k8s/base/exoplanets-api-service.yaml
-
 echo "Deployment completed successfully."
 
 if [[ "$current_context" == "docker-desktop" ]]; then
-  echo "Running on Docker Desktop. Proceeding with secrets creation..."
-  echo "Waiting for service 'exoplanets-api-service' to be ready..."
+  kubectl apply -f k8s/base/exoplanets-api-local-service.yaml
+
+  echo "Running on Docker Desktop. I have to forward the TCP ports to be able to connect with Exoplanet service."
+  echo "Checking if service 'exoplanets-api-service' is ready..."
 
   while true; do
     ready_pods=$(kubectl get pods --selector=app=exoplanets-api --field-selector=status.phase=Running --no-headers | wc -l)
@@ -37,9 +40,11 @@ if [[ "$current_context" == "docker-desktop" ]]; then
       echo "Service is ready. Proceeding with port-forwarding..."
       break
     fi
-    echo "Service not ready yet. Retrying in 2c seconds..."
+    echo "Service not ready yet. Retrying in 2 seconds..."
     sleep 2
   done
 
   kubectl port-forward service/exoplanets-api-service 8080:80
+else
+  kubectl apply -f k8s/base/exoplanets-api-cluster-service.yaml
 fi
